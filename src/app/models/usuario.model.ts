89866ef51d9278cc294d206227831ad4c7ac6345
Jose Tejada <jtejadavilca@gmail.com>
2019-06-22 01:01:29 -0500

    Avance inicial

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,17 @@
+// "id": 2,
+// "email": "janet.weaver@reqres.in",
+// "first_name": "Janet",
+// "last_name": "Weaver",
+// "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
+
+
+export class Usuario {
+    constructor(
+        public id: number,
+        public email: string,
+        public first_name: string,
+        public last_name: string,
+        public avatar) {
+
+    }
+}
---
+++ src/app/usuarios/lista/lista.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/usuario.model';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styles: []
+})
+export class ListaComponent implements OnInit {
+
+  usuarios: Usuario[] = [];
+
+  constructor( private usuarioService: UsuarioService ) { }
+
+  ngOnInit() {
+    this.usuarioService.geUsers()
+        .subscribe( users => {
+          console.log('users', users);
+          this.usuarios = users;
+        });
+  }
+
+}
